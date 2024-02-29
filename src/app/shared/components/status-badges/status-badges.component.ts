ccf003eaf4dd8d6293ab6bf7e4649e2ca88f7579
Mulyono Putra <mulyonoputra1994@gmail.com>
2024-02-29 22:03:02 +0700

    feat: Create reusable badges

---
+++ src/app/shared/components/status-badges/status-badges.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-status-badges',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './status-badges.component.html',
+  styleUrls: ['./status-badges.component.scss'],
+})
+export class StatusBadgesComponent {
+  @Input() status!: string;
+}
